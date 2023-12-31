c3999ee6e1da82eab58dd45d317bb327ccc88d90
Aditya Kumar Sinha <adityakumar113141@gmail.com>
2023-12-31 18:13:48 +0530

    Added Hamburger Icon

--- lib/src/side_menu_display_mode.dart
+++ lib/src/side_menu_display_mode.dart
@@ -17,3 +17,9 @@ enum SideMenuDisplayMode {
   /// of the content.
   compact
 }
+
+enum SideMenuHamburgerMode {
+  /// A hamburger icon appears in the top left of the side menu header. Clicking it opens or closes the side menu.
+  open,
+  close
+}
