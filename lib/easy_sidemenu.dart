7b8480b3b9689bbd5b984e26c865e214d7bf6383
elliotrtd <43793032+elliotrtd@users.noreply.github.com>
2025-06-11 19:10:43 +0800

    Update easy_sidemenu.dart

--- lib/easy_sidemenu.dart
+++ lib/easy_sidemenu.dart
@@ -6,3 +6,4 @@ export 'package:easy_sidemenu/src/side_menu_display_mode.dart';
 export 'package:easy_sidemenu/src/side_menu_item.dart';
 export 'package:easy_sidemenu/src/side_menu_expansion_item.dart';
 export 'package:easy_sidemenu/src/side_menu_style.dart';
+export 'package:easy_sidemenu/src/models/side_menu_item_type.dart';
